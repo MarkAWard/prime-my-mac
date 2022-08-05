935c7665322c824e061613dae32e487858208372
Mark Ward <markmaw@gmail.com>
2022-08-05 00:53:51 -0600

    customize prompt

--- files/dotfiles/zshrc.d/prompt.zsh
+++ files/dotfiles/zshrc.d/prompt.zsh
@@ -1,1 +1,15 @@
 # prompt settings
+
+prompt_time() {
+  prompt_segment white $PRIMARY_FG ' %* '
+}
+
+AGNOSTER_PROMPT_SEGMENTS=(
+  "prompt_status"
+#   "prompt_context"
+  "prompt_time"
+  "prompt_virtualenv"
+  "prompt_dir"
+  "prompt_git"
+  "prompt_end"
+)
